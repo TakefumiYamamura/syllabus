d60c3dd4cda983fa7c42bdf663586c4c06420063
TakeumiYamamura <yama1223xxx@sansontakeshi-no-MacBook-Air.local>
2015-02-03 15:20:14 +0900

    deviseとか導入

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,9 @@
 class Lesson < ActiveRecord::Base
   has_many :lessons
+  has_many :sikepuris
+  has_attached_file :kakomon,
+                    :styles =>  { medium: "100*100>", thumb: "100*100>" }
+
+  validates_attachment_content_type :kakomon,
+                    :content_type =>["image/jpeg","image/png","image/png","image/gi"]
 end
